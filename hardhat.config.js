1c265b22c22981dc64d0cbe5cf63316729cb65c0
Aayush Gupta <guptaaayush432@gmail.com>
2023-07-24 14:18:19 +0530

    Updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,38 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+require("./tasks")
+require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
-  solidity: "0.8.19",
-};
+    solidity: {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+            details: { yul: false },
+          },
+        },
+      },
+    defaultNetwork: "calibrationnet",
+    networks: {
+        calibrationnet: {
+            chainId: 314159,
+            url: "https://api.calibration.node.glif.io/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
+        filecoinmainnet: {
+            chainId: 314,
+            url: "https://api.node.glif.io",
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+}
